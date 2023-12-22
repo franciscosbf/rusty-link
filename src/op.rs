1d1701fcb4dcf71b3f4102214bf87f77c49677fd
Francisco Braço-Forte <f.s.f.b2001@gmail.com>
2023-12-22 16:18:25 +0000

    add ready op to operations module (remaining ops are missing)

--- src/op.rs
+++ src/op.rs
@@ -1,1 +1,35 @@
 //! TODO:
+
+#![allow(dead_code)] // TODO: remove this.
+
+use serde::Deserialize;
+
+#[derive(Deserialize, Debug)]
+pub(crate) struct ReadyOp {
+    pub(crate) resumed: bool,
+    #[serde(rename = "sessionId")]
+    pub(crate) session_id: String,
+}
+
+#[derive(Deserialize, Debug)]
+#[serde(tag = "op")]
+pub(crate) enum OpTypeOnly {
+    #[serde(rename = "ready")]
+    Ready(ReadyOp),
+}
+
+// TODO: remaining operations.
+
+#[derive(Deserialize, Debug)]
+#[serde(tag = "op")]
+pub(crate) enum OpType {
+    #[serde(rename = "ready")]
+    Ready(ReadyOp),
+    #[serde(rename = "playerUpdate")]
+    PlayerUpdate,
+    #[serde(rename = "stats")]
+    Stats,
+    #[serde(rename = "event")]
+    Event,
+}
+
