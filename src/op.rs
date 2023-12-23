2562016dc2eee3b884706ea14ef2227adc11d746
Francisco Braço-Forte <f.s.f.b2001@gmail.com>
2023-12-23 13:32:22 +0000

    add remaining web socket operations to op module

--- src/op.rs
+++ src/op.rs
@@ -4,11 +4,70 @@
 
 use serde::Deserialize;
 
+use crate::node::GuildId;
+use crate::model::{
+    PlayerState,
+    NodeStats,
+    TrackData,
+    TrackEndReason,
+    TrackException,
+    Milli,
+    DiscordAudioWsClosed,
+};
+
 #[derive(Deserialize, Debug)]
 pub(crate) struct ReadyOp {
     pub(crate) resumed: bool,
     #[serde(rename = "sessionId")]
     pub(crate) session_id: String,
+}
+
+#[derive(Deserialize, Debug)]
+pub(crate) struct UpdateOp {
+    #[serde(rename = "guildId")]
+    pub(crate) guild_id: GuildId,
+    pub(crate) state: PlayerState,
+}
+
+#[derive(Deserialize, Debug)]
+pub(crate) struct StatsOp {
+    #[serde(flatten)]
+    pub(crate) stats: NodeStats,
+}
+
+#[derive(Deserialize, Debug)]
+#[serde(tag = "type")]
+pub(crate) enum EventType {
+    #[serde(rename = "TrackStartEvent")]
+    TrackStart {
+        track: TrackData,
+    },
+    #[serde(rename = "TrackEndEvent")]
+    TrackEnd {
+        track: TrackData,
+        reason: TrackEndReason,
+    },
+    #[serde(rename = "TrackExceptionEvent")]
+    TrackException {
+        track: TrackData,
+        exception: TrackException,
+    },
+    #[serde(rename = "TrackStuckEvent")]
+    TrackStuck {
+        track: TrackData,
+        #[serde(rename = "thresholdMs")]
+        threshold: Milli,
+    },
+    #[serde(rename = "WebSocketClosedEvent")]
+    WebSocketClosed(DiscordAudioWsClosed),
+}
+
+#[derive(Deserialize, Debug)]
+pub(crate) struct EventOp {
+    #[serde(rename = "guildId")]
+    pub(crate) guild_id: GuildId,
+    #[serde(flatten)]
+    event: EventType,
 }
 
 #[derive(Deserialize, Debug)]
