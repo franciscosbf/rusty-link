af14c21674a19e2d15b66767167a124239693f1d
Francisco Braço-Forte <f.s.f.b2001@gmail.com>
2024-01-05 00:32:08 +0000

    add guild_id and node fields to PLayerRef and implement Player::new method

--- src/player.rs
+++ src/player.rs
@@ -5,15 +5,25 @@
 use std::sync::Arc;
 
 use crate::utils::InnerArc;
+use crate::node::{Node, GuildId};
 
 /// TODO:
 pub struct PlayerRef {
+    guild_id: GuildId,
+    node: Node,
 }
 
-/// TODO:
+/// Holds the original player so you can use as if it was protected by an [`Arc`] instance.
 #[derive(Clone)]
 pub struct Player {
     inner: Arc<PlayerRef>,
+}
+
+impl Player {
+    pub(crate) fn new(guild_id: GuildId, node: Node) -> Self {
+        let inner = PlayerRef { guild_id, node };
+        Self { inner: Arc::new(inner) }
+    }
 }
 
 impl InnerArc for Player {
